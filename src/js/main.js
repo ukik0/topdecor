7ae6eddeb95acfec3168215dc9bfe1e609ab7227
Dima <ctaricc@mail.ru>
2023-10-05 16:53:38 +0300

    refactor: change YMap settings

--- src/js/main.js
+++ src/js/main.js
@@ -457,15 +457,15 @@ if (document.querySelector('.map')) {
             }
         );
 
-        map.controls.remove('geolocationControl'); // удаляем геолокацию
-        map.controls.remove('searchControl'); // удаляем поиск
-        map.controls.remove('trafficControl'); // удаляем контроль трафика
-        map.controls.remove('typeSelector'); // удаляем тип
-        map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
-        map.controls.remove('zoomControl'); // удаляем контрол зуммирования
-        map.controls.remove('rulerControl'); // удаляем контрол правил
-
-        map.geoObjects.add(placemark);
+        // map.controls.remove('geolocationControl'); // удаляем геолокацию
+        // map.controls.remove('searchControl'); // удаляем поиск
+        // map.controls.remove('trafficControl'); // удаляем контроль трафика
+        // map.controls.remove('typeSelector'); // удаляем тип
+        // map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+        // map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+        // map.controls.remove('rulerControl'); // удаляем контрол правил
+        //
+        // map.geoObjects.add(placemark);
     }
 }
 
